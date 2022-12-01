9087fb8693ede8b96d3b8e45f2f58c17fce4b0ea
Dominguez <pilymdm@bu.edu>
2022-12-01 17:09:25 -0600

    feat: finish header

--- tindog-app/src/Header.js
+++ tindog-app/src/Header.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import './Header.css'
+import React from 'react';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ForumIcon from '@mui/icons-material/Forum';
+import { IconButton } from '@mui/material';
+import logo from './TINDog.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <IconButton>
+            <AccountBoxIcon className="header__icon" fontSize='large'/>
+        </IconButton>
+        <img className='header__logo' src={logo} alt="logo"/>
+        <IconButton>
+            <ForumIcon className="header__icon" fontSize='large'/>
+        </IconButton>
+        
+    </div>
   )
 }
 
