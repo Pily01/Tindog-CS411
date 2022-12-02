ef5f52d88506d8041f17a401cbe8e7f42cf8630f
Dominguez <pilymdm@bu.edu>
2022-12-02 14:50:01 -0600

    refactor: change image attribute to url

--- tindog-app/src/TinderCards.js
+++ tindog-app/src/TinderCards.js
@@ -8,11 +8,11 @@ function TinderCards() {
     const [pets, setPets] = useState([
         {
             name: 'Rocky',
-            image: 'https://i.natgeofe.com/n/5f35194b-af37-4f45-a14d-60925b280986/NationalGeographic_2731043_3x4.jpg'
+            url: 'https://i.natgeofe.com/n/5f35194b-af37-4f45-a14d-60925b280986/NationalGeographic_2731043_3x4.jpg'
         },
         {
             name: 'Max',
-            image: 'https://static01.nyt.com/images/2022/05/10/science/28DOGS-BEHAVIOR1/28DOGS-BEHAVIOR1-mobileMasterAt3x.jpg'
+            url: 'https://static01.nyt.com/images/2022/05/10/science/28DOGS-BEHAVIOR1/28DOGS-BEHAVIOR1-mobileMasterAt3x.jpg'
         }
     ])
 
@@ -23,7 +23,7 @@ function TinderCards() {
                 <TinderCard className='swipe' key={pet.name} preventSwipe={['up', 'down']}>
                     <div 
                     style = {{
-                        backgroundImage: `url(${pet.image})`
+                        backgroundImage: `url(${pet.url})`
                     }}
                     className='card'
                     >
