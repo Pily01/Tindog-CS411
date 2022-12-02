7aa87874c9196a68a02c4460fe7e68ff79effa1d
Dominguez <pilymdm@bu.edu>
2022-12-02 13:40:15 -0600

    feat: add react routes

--- tindog-app/src/App.js
+++ tindog-app/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Header from './Header';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Header/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
