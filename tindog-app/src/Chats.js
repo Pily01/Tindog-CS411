8fcecf1931e071517013d62db179ca4f011e1457
dua-ashfaq <duaashfaq22@gmail.com>
2022-12-08 19:21:42 -0500

    add chat style

--- tindog-app/src/Chats.js
+++ tindog-app/src/Chats.js
@@ -5,9 +5,9 @@ import Chat from "./Chat"
 function Chats(){
     return<div className = "chats">
         <Chat
-        name ="Ruby"
-        message ="Tailwag...Bark"
-        timestamp ="10 seconds"
+        name = "Ruby"
+        message = "Tailwag...Bark"
+        timestamp = "10 seconds"
         profilePic = "https://publish.purewow.net/wp-content/uploads/sites/2/2021/07/big-dog-breeds-bull-mastiff.jpg?fit=728%2C524"
         />
         <Chat
