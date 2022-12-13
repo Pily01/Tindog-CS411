73aba2f5d0bf9968a46e428e9382770b3b29f54f
Dominguez <pilymdm@bu.edu>
2022-12-12 18:24:42 -0600

    feat: add handleSend function

--- tindog-app/src/ChatScreen.js
+++ tindog-app/src/ChatScreen.js
@@ -4,7 +4,8 @@ import "./ChatScreen.css"
 import Avatar from "@material-ui/core/Avatar";
 
     function ChatScreen () {
-        const [messages, sentmessages] = useState([
+        const [input, setInput] = useState('');
+        const [messages, setmessages] = useState([
         {
             name: "Niko",
             image: "https://scontent.cdninstagram.com/v/t1.15752-9/318228277_5687429374679682_5543898317899502930_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5a057b&_nc_ohc=QHWKVKYyTF0AX8nCz_S&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AdSURZqLpWA74pa_uHGV86fjLQzZSI6aJNJwL5SW6CugbA&oe=63B715AF",
@@ -14,6 +15,13 @@ import Avatar from "@material-ui/core/Avatar";
             message: "woof! pant",
         },
     ])
+
+    const handleSend = e => {
+        e.preventDefault();
+        setmessages([...messages, {message: input}]);
+        setInput('');
+    }
+
         return(
             <div className = "chatScreen">
                <p className= "chatScreen_timestamp">You Matched With Niko On 11/08/22</p>
@@ -35,8 +43,12 @@ import Avatar from "@material-ui/core/Avatar";
                 ))}
                 
                     <form className='chatScreen__input'>
-                        <input className = 'chatScreen__inputField' type="text" placeholder='Type your message..woof!'/>
-                        <button className='chatScreen__inputButton'>SEND</button>
+                        <input 
+                        className = 'chatScreen__inputField'
+                        value={input}
+                        onChange = {(e) => setInput(e.target.value)}
+                        type="text" placeholder='Type your message..woof!'/>
+                        <button onClick={handleSend} type="submit" className='chatScreen__inputButton'>SEND</button>
                     </form>
                 
             </div>
